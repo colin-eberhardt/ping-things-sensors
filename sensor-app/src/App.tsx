df2361e2776469fe0501407e9748b1488af5a02d
Colin <eberhardt.colinj@gmail.com>
2023-06-25 11:33:47 -0400

    Stub Sensors page

--- sensor-app/src/App.tsx
+++ sensor-app/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Grid } from '@mui/material';
+import { Router } from 'react-router-dom';
 import PageCard from './components/PageCard';
 import PageHeader from './components/PageHeader';
 import './App.css';
+import Sensors from './pages/Sensors';
 
 const cardOptions = ["Issues", "Notifications", "Exploration", "Sensors"]
 
@@ -13,6 +15,7 @@ function App() {
       <Grid container spacing={0}>
         {cardOptions.map((cardOption:string) => <Grid item xs={8}><PageCard title={cardOption}/></Grid>)}
       </Grid>
+      <Sensors />
     </div>
   );
 }
